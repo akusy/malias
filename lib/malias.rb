d1eaa9d0d66a974af20de5be3b82926fdcee5cbb
meager <priorytet26@wp.pl>
2013-02-07 22:36:40 +0100

    create cli.rb

--- lib/malias.rb
+++ lib/malias.rb
@@ -1,45 +1,5 @@
 require "malias/version"
 
 module Malias
-  class Cli < Thor
-    include Thor::Actions
-
-    desc "make", "create alias"
-    method_option :file, :aliases => "-f"
-
-    def make (name, path)
-      if target
-        append_file target, "alias #{name}=\'#{path}\'"
-        run("source #{target}")
-      else
-        say "you don't have alias file", :red
-      end
-    end
-
-    private
-
-    def target
-      userfile || bashrc || bashprofile
-    end
-
-    def homedir
-      Pathname.new(Dir.home)
-    end
-
-    def bashrc
-      file = homedir.join('.bashrc')
-      file if file.exist?
-    end
-
-    def bashprofile
-      file = homedir.join('.bash_profile')
-      file if file.exist?
-    end
-
-    def userfile
-      return unless options[:file]
-      file = Pathname.new(options[:file])
-      file if file.exist?
-    end
-  end
+  
 end
